03aa812d19d1f505ef6609eb24f2e95c94bba117
hemalak <101514288+hemalak@users.noreply.github.com>
2022-07-30 21:57:28 +0530

    Add files via upload

---
+++ ai_4th_task.py
@@ -0,0 +1,6 @@
+E = {0, 1, 2, 3, 4}
+N = {5, 6, 8 ,2 ,4}
+print("Union of E and N is",E | N)
+print("Intersection of E and N is",E & N)
+print("Difference of E and N is",E - N)
+print("Symmetric difference of E and N is",E ^ N)
